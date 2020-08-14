201c494d8b7f3a0e8c7f75cbd68bab82467779c8
Dominik Vach <vach.dominik@gmail.com>
2020-08-14 16:23:27 +0200

    move files + generate pypi structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dbhydra",
+    version="0.1.0",
+    author="DovaX",
+    author_email="dovax.ai@gmail.com",
+    description="Data science friendly ORM combining Python, Pandas, and various SQL dialects ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DovaX/dbhydra",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
