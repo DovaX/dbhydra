f60d8f71e39e716176beb062f615e171741a1cae
tsai00 <tsai00@vse.cz>
2022-10-23 17:55:13 +0200

    Fix adjust df method

--- dbhydra/dbhydra_core.py
+++ dbhydra/dbhydra_core.py
@@ -570,7 +570,7 @@ class AbstractTable(AbstractJoinable):
 
     def adjust_df(self, df: pd.DataFrame, debug_mode=False) -> pd.DataFrame:
         """
-        Adjust DataFrame in case number iof its columns differs from number of columns in DB table.
+        Adjust DataFrame in case number of its columns differs from number of columns in DB table.
         :param df: original DF
         :param debug_mode: in debug mode a warning will be printed to console
         :return: adjusted DF
@@ -578,24 +578,22 @@ class AbstractTable(AbstractJoinable):
 
         df_copy = df.copy()
 
-        # DataFrame has less columns
-        if len(self.columns) - len(df_copy.columns) > 1:
-            difference_columns = list(set(self.columns).difference(set(df_copy.columns)).difference(set(['id'])))
+        difference_columns_missing = list(set(self.columns).difference(set(df_copy.columns)).difference({'id'}))
 
+        if difference_columns_missing:
             if debug_mode:
-                print(f'DataFrame contains less columns than DB table. Missing columns {difference_columns} will be added as empty.')
+                print(f'DataFrame missing columns {difference_columns_missing} will be added as empty.')
 
-            for col in difference_columns:
+            for col in difference_columns_missing:
                 df_copy[col] = np.nan
 
-        # DataFrame has more columns
-        elif len(self.columns) < len(df_copy.columns):
-            difference_columns = list(set(df_copy.columns).difference(set(self.columns)))
+        difference_columns_extra = list(set(df_copy.columns).difference(set(self.columns)))
 
+        if difference_columns_extra:
             if debug_mode:
-                print(f'DataFrame contains more columns than DB table. Extra columns {difference_columns} will be removed.')
+                print(f'DataFrame extra columns {difference_columns_extra} will be removed.')
 
-            df_copy.drop(difference_columns, axis=1, inplace=True)
+            df_copy.drop(difference_columns_extra, axis=1, inplace=True)
 
         # Ensure the same order of columns in DF as in DB table
         cols_order = [x for x in self.columns if x != 'id']
