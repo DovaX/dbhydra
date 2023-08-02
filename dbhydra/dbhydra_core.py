e97e7bf6eaf61be4b174ec9ea3937f4ef9994431
Dominik Vach <vach.dominik@gmail.com>
2023-08-02 20:51:18 +0200

    improve xlsx db - release 1.2.4

--- dbhydra/dbhydra_core.py
+++ dbhydra/dbhydra_core.py
@@ -7,6 +7,7 @@ import sys
 import threading
 from contextlib import contextmanager
 from sys import platform
+import os
 
 import numpy as np
 import pandas as pd
@@ -767,9 +768,14 @@ class AbstractTable(AbstractJoinable, abc.ABC):
 
     # Temporary disabled, please make sure this is implemented where needed, don't introduce breaking changes please
     # @abc.abstractmethod
-    def init_from_column_type_dict(db1, name, column_type_dict, init_from_column_type_dict="id"):
-        pass
-
+    @classmethod
+    def init_from_column_type_dict(cls, db1, name, column_type_dict, id_column_name="id"):
+        column_converted_type_dict = db1._convert_column_type_dict_from_python(column_type_dict)
+        columns = list(column_converted_type_dict.keys())
+        types = list(column_converted_type_dict.values())
+        return cls(db1, name, columns, types, id_column_name=id_column_name)
+    
+    
     def drop(self):
         query = "DROP TABLE " + self.name
         print(query)
@@ -1402,12 +1408,13 @@ class MysqlTable(MysqlSelectable, AbstractTable):
         self.types = types
         self.id_column_name = id_column_name
 
-    @classmethod
-    def init_from_column_type_dict(cls, db1, name, column_type_dict, id_column_name="id"):
-        column_converted_type_dict = db1._convert_column_type_dict_from_python(column_type_dict)
-        columns = list(column_converted_type_dict.keys())
-        types = list(column_converted_type_dict.values())
-        return cls(db1, name, columns, types, id_column_name=id_column_name)
+    #Disabled because it is inherited
+    # @classmethod
+    # def init_from_column_type_dict(cls, db1, name, column_type_dict, id_column_name="id"):
+    #     column_converted_type_dict = db1._convert_column_type_dict_from_python(column_type_dict)
+    #     columns = list(column_converted_type_dict.keys())
+    #     types = list(column_converted_type_dict.values())
+    #     return cls(db1, name, columns, types, id_column_name=id_column_name)
 
     def initialize_columns(self):
         information_schema_table = Table(self.db1, 'INFORMATION_SCHEMA.COLUMNS')
@@ -1661,9 +1668,24 @@ class MysqlTable(MysqlSelectable, AbstractTable):
             print("Cant modify column to table.")
 
 
-class XlsxDB:
-    def __init__(self, name="new_db", config_file="config.ini"):
-        self.name = name
+class XlsxDB(AbstractDB):
+    def __init__(self, config_file="config.ini", db_details=None):
+        if db_details is None:
+            self.name="new_db"
+        else:
+            self.name = db_details.get("DB_DATABASE")
+        self.lock = threading.Lock()
+        
+        self.python_database_type_mapping = {
+        'int': "int",
+        'float': "double",
+        'str': "str",
+        'list': "str",
+        'dict': "str",
+        'bool': "bool",
+        'datetime': "datetime"
+        }
+
 
         """
         db_details=read_connection_details(config_file)
@@ -1684,6 +1706,31 @@ class XlsxDB:
             self.DB_PASSWORD = db_details["DB_PASSWORD"]
             self.connect_remotely()
         """
+        
+        class DummyXlsxConnection: #compatibility with MySQL connection
+            def begin(*args):
+                pass
+            def commit(*args):
+                pass
+            def rollback(*args):
+                pass
+            
+        class DummyXlsxCursor: #compatibility with MySQL connection
+            def execute(*args):    
+                pass
+            
+            def fetchall(*args):
+                pass
+            
+        self.cursor=DummyXlsxCursor()
+        self.connection=DummyXlsxConnection()
+        self.create_database()
+        
+    def connect_locally(self):
+        pass #no real connection
+        
+    def connect_remotely(self):
+        pass #no real connection
 
     def execute(self, query):
         pass
@@ -1696,18 +1743,26 @@ class XlsxDB:
         # print("DB connection closed")
 
     def create_database(self):
-        import os
         try:
             os.mkdir(self.name)
             print("Database created")
-        except:
+        except FileExistsError:
             print("Database is already created")
 
 
 class XlsxTable(AbstractTable):
-    def __init__(self, db1, name, columns=None, types=None):
+    def __init__(self, db1, name, columns=None, types=None, id_column_name = "id"):
         super().__init__(db1, name, columns)
         self.types = types
+        self.id_column_name=id_column_name
+        
+        
+    def create(self):
+        df=pd.DataFrame(columns=self.columns)
+        df.to_excel(self.db1.name + "\\" + self.name + ".xlsx",index=False)
+    
+    def drop(self):
+        pass
 
     def select_to_df(self):
         try:
@@ -1727,33 +1782,39 @@ class XlsxTable(AbstractTable):
 
         original_df = self.select_to_df()
 
+        original_df.index=original_df[self.id_column_name]
         try:
             original_df = original_df.drop(original_df.columns[0], axis=1)
         except:
             print("First column could not be dropped")
 
-        df.columns = original_df.columns
         # handling nan values -> change to NULL TODO
         for column in list(df.columns):
             df.loc[pd.isna(df[column]), column] = "NULL"
 
         def concat_with_reset_index_in_second_df(original_df, df):
             """Subsitute of reset_index method because we need to maintain the ids of original df"""
-            maximal_index = max(original_df.index)
+            if len(original_df.index)>0:
+                maximal_index = max(original_df.index)
+            else:
+                maximal_index=0
             df.index = df.index + maximal_index + 1
+            df.reindex(columns=original_df.columns.tolist()) #to ensure that columns get correctly inserted
             original_df = original_df.append(df)
             # df=pd.concat([original_df,df])
             return (original_df)
 
-        if len(original_df) > 0:
-            df = concat_with_reset_index_in_second_df(original_df, df)
-
-        df.to_excel(self.db1.name + "\\" + self.name + ".xlsx")
+        df = concat_with_reset_index_in_second_df(original_df, df)
+        df["uid"]=df.index
+        df.reindex(columns=["uid"]+df.columns[:-1].tolist()) #uid as a first column
+        df.reset_index(drop=True,inplace=True)
+        print("INSERTED",df)
+        df.to_excel(self.db1.name + "\\" + self.name + ".xlsx",index=False)
 
     def replace_from_df(self, df):
         assert len(df.columns) == len(self.columns)  # +1 because of id column
-        df.drop(df.columns[0], axis=1, inplace=True)
-        df.to_excel(self.db1.name + "\\" + self.name + ".xlsx")
+        #df.drop(df.columns[0], axis=1, inplace=True)
+        df.to_excel(self.db1.name + "\\" + self.name + ".xlsx",index=False)
 
     def update(self, variable_assign, where=None):
         def split_assign(variable_assign):
@@ -1774,7 +1835,7 @@ class XlsxTable(AbstractTable):
             print(df)
         else:
             where_variable, where_value = split_assign(where)
-            df[variable] = df[where_variable].apply(lambda x: value if x == where_value else x)
+            df[variable] = df[where_variable].apply(lambda x: value if str(x) == str(where_value) else x) #
         self.replace_from_df(df)
 
     def delete(self, where=None):
