3240f2798168b72b219c77f42232d9136bce3459
Jakub Kawecki <jakub@forloop.ai>
2023-09-13 16:41:29 +0200

    Bugfix: XlsxDB now loads strings with dtype=object, so they hopefully are not parsed while loading excel sheet

--- dbhydra/dbhydra_core.py
+++ dbhydra/dbhydra_core.py
@@ -1844,12 +1844,14 @@ class XlsxTable(AbstractTable):
 
 
     def select_to_df(self):
+        # String must be typed with 'object' otherwise they get parsed arbitrarily and
+        # then cast to string, e.g. 'true' -> True -> 'True'
+        column_type_map = {
+            column: object for column, type_ in zip(self.columns, self.types) if type_ == "str"
+        }
+
         try:
-            df = pd.read_excel(self.table_directory_path)
-            # cols=df.columns
-            # print(cols)
-            # df.set_index(cols[0],inplace=True)
-            # df.drop(df.columns[0],axis=1,inplace=True)
+            df = pd.read_excel(self.table_directory_path, dtype=column_type_map)
             df.replace({np.nan: None}, inplace=True)
         except Exception as e:
             print("Error: ", e)
@@ -1867,7 +1869,7 @@ class XlsxTable(AbstractTable):
         except:
             print("First column could not be dropped")
 
-        # handling nan values -> change to NULL TODO
+        # handling nan values -> change to NULL TODO  
         for column in list(df.columns):
             df.loc[pd.isna(df[column]), column] = None
 
