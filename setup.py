8aafad26340234e44a048a783227dc1270a3b15f
Dominik Vach <vach.dominik@gmail.com>
2024-01-21 15:43:35 +0100

    release 2.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='dbhydra',
-    version='2.0.12',
+    version='2.0.13',
     author='DovaX',
     author_email='dovax.ai@gmail.com',
     description='Data science friendly ORM combining Python',
