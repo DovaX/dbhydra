531ea69595d2319e9eb8d0941cee2b8f061bdd11
Jakub Kawecki <jakub@forloop.ai>
2024-06-06 13:07:48 +0200

    Introduce BLOB type to MySQL

--- dbhydra/src/tables.py
+++ dbhydra/src/tables.py
@@ -5,9 +5,9 @@ import abc
 import time
 #xlsx imports
 import pathlib
-
+import pymysql
 from dbhydra.src.abstract_table import AbstractTable, AbstractSelectable, AbstractJoinable
-
+import binascii
 
 MONGO_OPERATOR_DICT = {"=": "$eq", ">": "$gt", ">=": "$gte", " IN ": "$in", "<": "$lt", "<=": "$lte", "<>": "$ne"}
 
@@ -807,7 +807,11 @@ class MysqlTable(AbstractTable):
                     query += "'" + str(rows[k][j]) + "',"
                 elif "json" in self.types[j + start_index]:
                     query += f"'{rows[k][j]}', "
-
+                elif 'blob' in self.types[j + start_index]:
+                    escaped_binary_data = pymysql.Binary(rows[k][j])
+                    # Convert to hex to allow insertion into SQL query
+                    hex_data = binascii.hexlify(escaped_binary_data).decode('ascii')
+                    query += f"UNHEX('{hex_data}'), "
 
                 else:
                     query += str(rows[k][j]) + ","
